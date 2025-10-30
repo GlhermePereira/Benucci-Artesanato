1823e8761eed901fa9b41357888e60bf8cd5a4ff
GlhermePereira <guilherme.aqw24@gmail.com>
2025-10-30 20:32:50 -0300

    Refatora SwaggerConfig: ajusta versão para 1.0.0, organiza código e mantém segurança JWT

--- src/main/java/br/edu/fatecpg/BenucciArtesanato/config/SwaggerConfig.java
+++ src/main/java/br/edu/fatecpg/BenucciArtesanato/config/SwaggerConfig.java
@@ -2,24 +2,38 @@ package br.edu.fatecpg.BenucciArtesanato.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
-
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+
+        Info apiInfo = new Info()
+                .title("API E-commerce")
+                .version("1.0.0")
+                .description("API E-commerce com Swagger via ngrok");
+
+
+        Components components = new Components()
+                .addSecuritySchemes("bearer-key",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                );
+
+        SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearer-key");
+
         return new OpenAPI()
-                .info(new Info().title("API E-commerce").version("1.0").description("API E-commerce com Swagger via ngrok"))
-                .components(new Components()
-                        .addSecuritySchemes("bearer-key",
-                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
-                        ))
-                .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+                .info(apiInfo)
+                .components(components)
+                .addSecurityItem(securityRequirement);
     }
 }
