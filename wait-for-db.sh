aea7d8e1579b41b68a20eef6eebb4666565635cf
GlhermePereira <guilherme.aqw24@gmail.com>
2025-12-05 10:30:21 -0300

    feat(delete): limpando o diretorio

--- wait-for-db.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Espera o PostgreSQL ficar disponível
-until pg_isready -h "${DATABASE_URL#*//}" -p 5432 -U "$DATABASE_USER"; do
-  echo "Esperando banco de dados em $DATABASE_URL..."
-  sleep 2
-done
-
-# Inicia a aplicação
-exec java -jar target/Benucci-App-0.0.1-SNAPSHOT.jar
